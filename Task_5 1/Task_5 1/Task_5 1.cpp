7770e54edc02c12ce0e5c5e3cf1719ba524f7686
uny2001 <139007547+uny2001@users.noreply.github.com>
2023-07-13 21:43:02 +0300

    Add files via upload
    Задание 5.7

---
+++ Task_5 1/Task_5 1/Task_5 1.cpp
@@ -0,0 +1,20 @@
+// Task_5 1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian_Russia.1251");
+	
+
+	for (int i = 0; i < 20; i++)
+{
+cout << 20 << " ";
+}
+	system("pause");
+	return 0;
+}
